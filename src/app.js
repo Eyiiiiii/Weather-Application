0e085770783ebe0d157d271fbf48a87c0a89c833
Eyiiiiii <133078366+Eyiiiiii@users.noreply.github.com>
2023-06-19 00:28:34 -0200

    weather API replaced by the weather API SheCodes, needs to get modification

--- src/app.js
+++ src/app.js
@@ -1,23 +1,26 @@
 function showTemp(value) {
   let updateTemp = document.querySelector("#temperature");
-  updateTemp.innerHTML = Math.round(value.data.main.temp);
+  updateTemp.innerHTML = Math.round(value.data.temperature);
 
   let updateCity = document.querySelector("#city");
-  updateCity.innerHTML = value.data.name;
+  updateCity.innerHTML = value.data.city;
 
   let updateDesc = document.querySelector("#description");
-  updateDesc.innerHTML = value.data.weather[0].description;
+  updateDesc.innerHTML = value.data.condition.description;
 
   let updateWindsp = document.querySelector("#weather-Wind");
   updateWindsp.innerHTML = Math.round(value.data.wind.speed);
 
   let updatePreci = document.querySelector("#weather-Preci");
-  updatePreci = value.data.clouds.all;
+  updatePreci.innerHTML = value.data.condition.precipitation;
 
   let updateHumi = document.querySelector("#weather-Humi");
-  updateHumi.innerHTML = value.data.main.humidity;
+  updateHumi.innerHTML = value.data.temperature.humidity;
 
-  let currentTime = new Date();
+  let updateIcon = document.querySelector("#icon");
+  updateIcon.setAttribute("src", value.data.condition.icon_url);
+
+  let currentTime = new Date(value.data.time * 1000); // Convert timestamp to milliseconds
   let hours = currentTime.getHours();
   let minutes = currentTime.getMinutes();
   let days = [
@@ -36,8 +39,9 @@ function showTemp(value) {
   updatedTime.innerHTML = timeString;
 }
 
-let apiKey = "f3887e262c88d1158f7e2ef4998e234c";
+let apiKey = "605tfadf3cb16c3770fb6d9dc43a237o";
+let city = "Lisbon";
 let units = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cebu&appid=${apiKey}&units=${units}`;
+let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
 
 axios.get(apiUrl).then(showTemp);
