a9a5fd32749dcd622780ba58ec81e8f0f6951e04
Eyiiiiii <133078366+Eyiiiiii@users.noreply.github.com>
2023-06-24 16:20:52 -0200

    added conversion functions, and changed the bg img

--- Code/src/app.js
+++ Code/src/app.js
@@ -1,6 +1,6 @@
 function showTemp(response) {
   let updateTemp = document.querySelector("#temperature");
-  updateTemp.innerHTML = Math.round(response.data.temperature.current);
+  updateTemp.innerHTML = Math.round(celTemp);
 
   let updateCity = document.querySelector("#city");
   updateCity.innerHTML = response.data.city;
@@ -19,7 +19,8 @@ function showTemp(response) {
 
   let updateIcon = document.querySelector("#icon");
   updateIcon.setAttribute("src", response.data.condition.icon_url);
-  updateIcon.setAttribute("alt", response.data.condition[0]);
+
+  celTemp = response.data.temperature.current;
 
   let currentTime = new Date(response.data.time * 1000);
   let hours = currentTime.getHours();
@@ -52,5 +53,26 @@ function submitBtn(event) {
   search(cityUpdate.value);
 }
 
+let celTemp = null;
+
+function showFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheiTemp = (celTemp * 9) / 5 + 32;
+  let updateTemp = document.querySelector("#temperature");
+  updateTemp.innerHTML = Math.round(fahrenheiTemp);
+}
+
+function showCelcius(event) {
+  event.preventDefault();
+  let updateTemp = document.querySelector("#temperature");
+  updateTemp.innerHTML = Math.round(celTemp);
+}
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", submitBtn);
+
+let fahrenheit = document.querySelector("#fah");
+fahrenheit.addEventListener("click", showFahrenheit);
+
+let celcius = document.querySelector("#cel");
+celcius.addEventListener("click", showCelcius);
